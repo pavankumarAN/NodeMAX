140fd837f0d77bbd360b79fde6d8901d4b2f3a33
PavankumarAN <pavankumaran51@gmail.com>
2019-08-01 07:52:44 +0530

    MySQL connection

---
+++ MySQLCon/util/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node_complete',
+    password: 'Pavan@link15'
+});
+
+module.exports = pool.promise();
--- NodeAsApp/app.js
+++ NodeAsApp/app.js
@@ -2,33 +2,33 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-
+const users = [];
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 
 app.use(bodyParser.urlencoded({extended:false}));
-const users = [];
+
 
 
 app.get('/', (req,res,next) => {
     res.render('index', {
-        pageTitle : 'Users'
+        pageTitle : 'Add users'
     });
 });
 
 app.get('/users', (req,res,next) => {
     res.render('users', {
-        pageTitle : 'Add users' ,
-        users:users, 
+        pageTitle : 'Users',
+        users: users, 
         hasUsers : users.length > 0
     });
 });
 
 app.post('/add-user', (req,res,next) => {
     users.push({name : req.body.username});
-    res.render('users');
+    res.render('users', {users});
 });
 
 app.listen(3022);
