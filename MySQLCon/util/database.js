7027bdc4471fb39b808e39360e7ba910fbb79e73
PavankumarAN <pavankumaran51@gmail.com>
2019-08-05 07:51:30 +0530

    Connection fixed

--- MySQLCon/util/database.js
+++ MySQLCon/util/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     database: 'node_complete',
-    password: 'LearnWell@123'
+    password: 'Pavan@link15'
 });
 
 module.exports = pool.promise();
